942554cc0ea86675dfdce74170473b76de9cf110
lim-jiahao <lim.jiahaoo@gmail.com>
2021-10-20 18:39:04 +0800

    comfortable

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+const msgToUser = 'USAGE: node index.js hexrgb|rgbhex <hex/rgb color code>\
+                    \ne.g. node index.js hexrgb #ffffff\
+                    \ne.g. node index.js rgbhex rgb(255, 255, 255)';
+
 const hexToRGB = (hex) => {
   let base16 = hex.slice(1);
   if (base16.length === 3) base16 = base16[0].repeat(2) + base16[1].repeat(2) + base16[2].repeat(2);
@@ -9,10 +13,18 @@ const hexToRGB = (hex) => {
   return `rgb(${red}, ${green}, ${blue})`;
 }
 
-let hexCodeRegex = /^#(?:[0-9a-fA-F]{3}){1,2}$/;
+const rgbToHex = (rgb) => {
+  let rgbSplits = rgb.slice(4).split(',');
+  let [red, green, blue] = rgbSplits.map((val) => parseInt(val, 10).toString(16));
+  return `#${red}${green}${blue}`;
+}
 
-if (process.argv.length === 3 && hexCodeRegex.test(process.argv[2])) {
-  console.log(hexToRGB(process.argv[2]));
+// let hexCodeRegex = /^#(?:[0-9a-fA-F]{3}){1,2}$/;
+
+if (process.argv.length === 4) {
+  if (process.argv[2] === 'hexrgb') console.log(hexToRGB(process.argv[3]));
+  else if (process.argv[2] === 'rgbhex') console.log(rgbToHex(process.argv[3]));
+  else console.log(msgToUser)
 } else {
-  console.log('USAGE: node index.js <hex color code> e.g. node index.js #ffffff')
+  console.log(msgToUser)
 }
